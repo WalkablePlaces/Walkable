40fdd34f7d8c4d361d87481bc018feab895f81a4
PrincePuggo <108048379+PrincePuggo@users.noreply.github.com>
2023-06-09 16:50:01 -0700

    baseline setup

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const cors = require('cors');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log('listing on 3000'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,70 @@
+/* eslint-disable no-unused-vars */
+const webpack = require('webpack');
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
-    entry: [ 
-        'babel-polyfill',
-       path.resolve(__dirname, './src/index.js'),
-
-    ],
-
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js',
-
+  entry: [
+    // entry point of our app
+    './client/index.js',
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    // match the output path
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      // match the output 'publicPath'
+      publicPath: '/',
     },
-    devServer: {
-        host: 'localhost',
-        port: 8080,
-        // match the output path
-        static: {
-          directory: path.resolve(__dirname, 'dist'),
-          // match the output 'publicPath'
-          publicPath: '/',
-        },
-        // enable HMR on the devServer
-        hot: true,
-        // fallback to root for other urls
-        historyApiFallback: true,
-        headers: { 'Access-Control-Allow-Origin': '*' },
-        /**
-         * proxy is required in order to make api calls to
-         * express server while using hot-reload webpack server
-         * routes api fetch requests from localhost:8080/api/* (webpack dev server)
-         * to localhost:3000/api/* (where our Express server is running)
-         */
-        proxy: {
-          '/api/**': {
-            target: 'http://localhost:3000/',
-            secure: false,
-          },
-          '/assets/**': {
-            target: 'http://localhost:3000/',
-            secure: false,
-          },
-        },
+    // enable HMR on the devServer
+    hot: true,
+    // fallback to root for other urls
+    historyApiFallback: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    /**
+     * proxy is required in order to make api calls to
+     * express server while using hot-reload webpack server
+     * routes api fetch requests from localhost:8080/api/* (webpack dev server)
+     * to localhost:3000/api/* (where our Express server is running)
+     */
+    proxy: {
+      '/api/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+    },
+  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
       },
-
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html'
-        })
-    ],
-
-    module: {
-        rules: [
-            {
-                test: /.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            }
-        ]
-    },
-    resolve: {
-        // Enable importing JS / JSX files without specifying their extension
-        extensions: ['.js', '.jsx'],
-    },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
+  resolve: {
+    // Enable importing JS / JSX files without specifying their extension
+    extensions: ['.js', '.jsx'],
+  },
 }

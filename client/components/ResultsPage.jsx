aece6ca4b782b6dd7294a9a6af8ec84eb2e757a5
Garrett Allen <garrettallen@Garretts-MBP.lan>
2023-06-09 20:10:35 -0700

    updates results page co-author: https://github.com/PrincePuggo

--- client/components/ResultsPage.jsx
+++ client/components/ResultsPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import MainContainer from './MainContainer.jsx'
 
 export default function ResultsPage() {
 
   
   return (
-    <div>hello</div>
+    <div>
+    <h1>Results within walking distance</h1>
+    <MainContainer />
+    </div>
   )
 }
